dd6f88924d2ca7d60f2994f70d89ce7b32642ac4
djakovm <martindjakov03@gmail.com>
2024-08-27 23:37:44 +0200

    Stashing changes

--- .godot/mono/temp/obj/Debug/FinkiAdventureQuest.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/FinkiAdventureQuest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinkiAdventureQuest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3284785797b83f91fa5da19787e036791e0c0b87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2deb0827e07366bf2e86a52abaff9e8bbe54ab44")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinkiAdventureQuest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinkiAdventureQuest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
