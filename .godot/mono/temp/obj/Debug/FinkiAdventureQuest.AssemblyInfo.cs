d04aa3c274fb5878406cf8dec336eb4e1f21c101
djakovm <martindjakov03@gmail.com>
2024-09-01 23:36:29 +0200

    merge

--- .godot/mono/temp/obj/Debug/FinkiAdventureQuest.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/FinkiAdventureQuest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinkiAdventureQuest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d09940512314ddabb9e33a34ec34e3344d99147")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b172f0073b4a041cec1652e434413cec35d25e2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinkiAdventureQuest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinkiAdventureQuest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
